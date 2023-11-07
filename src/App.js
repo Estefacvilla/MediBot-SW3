cedd9e1e25c53a3db8fe569ed6b652b2a571722b
Estefanía Castaño <estefania.1701212869@ucaldas.edu.co>
2023-11-07 15:12:32 -0500

    Guardar cambios locales antes de cambiar de rama

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
+import Body from './components/body';
+import Chatbot from './Chatbot';
 
 function App() {
-  const numero = "+573005186859";
-  const texto = "Hola, quiero información";
-  const url = `https://wa.me/${numero}?text=${texto}`;
-
   return (
     <div>
       <Navbar />
